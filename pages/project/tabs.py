228dc408cb84f1e458859f57ffb2432e0a68bd4c
3lectronicbody <dominik.jarmocik@gmail.com>
2026-06-18 10:06:16 +0200

    Fix `load_data` rendering and improve empty list handling in To-Do Tab
    - Resolved issues with `empty_list_label` persistence when the To-Do list is updated.
    - Ensured proper memory management by safely deleting `empty_list_label` when items exist.
    - Enhanced alignment for the "No To-Do List" message using `Qt.AlignmentFlag.AlignCenter`.
    TODO: Refractor ToDoTab.load_data()

--- pages/project/tabs.py
+++ pages/project/tabs.py
@@ -4,6 +4,7 @@ from database.models import ProjectDetails
 from helper_functions import clear_layout
 from custom_widgets import EditItem, EditActivity, EditToDo
 from helper_functions import confirmation_dialog
+from PySide6.QtGui import Qt
 
 
 
@@ -142,11 +143,10 @@ class ToDoTab(QWidget):
         self.database = database
         self.project_id = project_id
         self.user_id = user_id
+        self.empty_list_label = None
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-
-
 
         self.main_layout = QGridLayout()
         self.layout.addLayout(self.main_layout)
@@ -155,31 +155,35 @@ class ToDoTab(QWidget):
 
     def load_data(self):
         clear_layout(self.main_layout, grid_layout=True)
-        # clear_layout(self.layout, grid_layout=False)
         with self.database.session() as session:
             project_details = session.query(ProjectDetails).filter_by(project_id=self.project_id).all()
             items = [detail for detail in project_details if detail.todo is not None]
-            if not items:
-                empty_list_label = QLabel("No To-Do List")
-                self.layout.addWidget(empty_list_label)
+            if not items and self.empty_list_label is None:
+                self.empty_list_label = QLabel("No To-Do List")
+                self.layout.addWidget(self.empty_list_label, alignment=Qt.AlignmentFlag.AlignCenter)
                 return
-            counter = 1
-            for index, item in enumerate(items, start=1):
-                id_label = QLabel(str(index))
-                self.main_layout.addWidget(id_label, index-1,0)
-                todo_label = QLabel(item.todo)
-                self.main_layout.addWidget(todo_label, index-1,1)
-                quantity_label = QLabel(str(item.quantity))
-                self.main_layout.addWidget(quantity_label, index-1,2)
-                complete_button = QPushButton("Complete")
-                complete_button.clicked.connect(lambda _, item_id = item.id: self.complete_button_handler(item_id))
-                self.main_layout.addWidget(complete_button, index-1,3)
-                edit_button = QPushButton("Edit")
-                edit_button.clicked.connect(lambda _, item_id = item.id: self.edit_button_handler(item_id))
-                self.main_layout.addWidget(edit_button, index-1,4)
+            else:
+                if self.empty_list_label:
+                    self.layout.removeWidget(self.empty_list_label)
+                    self.empty_list_label.deleteLater()  # Safely wipe it from memory
+                    self.empty_list_label = None
+                counter = 1
+                for index, item in enumerate(items, start=1):
+                    id_label = QLabel(str(index))
+                    self.main_layout.addWidget(id_label, index-1,0)
+                    todo_label = QLabel(item.todo)
+                    self.main_layout.addWidget(todo_label, index-1,1)
+                    quantity_label = QLabel(str(item.quantity))
+                    self.main_layout.addWidget(quantity_label, index-1,2)
+                    complete_button = QPushButton("Complete")
+                    complete_button.clicked.connect(lambda _, item_id = item.id: self.complete_button_handler(item_id))
+                    self.main_layout.addWidget(complete_button, index-1,3)
+                    edit_button = QPushButton("Edit")
+                    edit_button.clicked.connect(lambda _, item_id = item.id: self.edit_button_handler(item_id))
+                    self.main_layout.addWidget(edit_button, index-1,4)
 
-                counter += 1
-            self.main_layout.setRowStretch(counter, 1)
+                    counter += 1
+                self.main_layout.setRowStretch(counter, 1)
     def complete_button_handler(self, item_id):
         with self.database.session() as session:
             item = session.query(ProjectDetails).get(item_id)
