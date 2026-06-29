e22e2a476aef795c8bd733d0e05dbc0bc61a9451
3lectronicbody <dominik.jarmocik@gmail.com>
2026-06-29 15:57:27 +0200

    Set consistent layout margins and improve styling across widgets
    - Added a uniform `setContentsMargins(0, 0, 0, 0)` to `MainFrame` and `ProjectWindow` layouts.
    - Updated `MenuBar` widgets with a new background color and border-radius styling.
    - Commented out unused style sheet for `projects_button`.

--- pages/service_projects_page.py
+++ pages/service_projects_page.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QGridLayout
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QComboBox
+from database.models import ServiceProjects
+from helper_functions import clear_layout
 
 
 class ServiceProjectsPage(QWidget):
@@ -8,12 +10,29 @@ class ServiceProjectsPage(QWidget):
         self.database = database
         self.parent = parent
 
-        self.main_layout = QGridLayout()
+        self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
+
+        self.dropdown_service_projects = QComboBox()
+        options = [ "all","active", "complete",]
+        self.dropdown_service_projects.addItems(options)
+        self.dropdown_service_projects.setCurrentIndex(0)
+
+        self.data_layout = QGridLayout()
+        self.main_layout.addLayout(self.data_layout)
+
+
+
 
 
 
     def refresh_data(self):
-        pass
+        clear_layout(self.data_layout, grid_layout=True)
+
+        with self.database.session() as session:
+            projects = session.query(ServiceProjects).all()
+            for index, project in enumerate(projects):
+
+
 
 
